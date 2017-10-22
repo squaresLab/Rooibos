8726309c1b13d9d4a7a9ce37fb60843630ab3bfc
ChrisTimperley <christimperley@googlemail.com>
2017-10-21 21:47:17 -0400

    added Fragment.print

--- src/fragment.ml
+++ src/fragment.ml
@@ -1,13 +1,21 @@
 (* I don't like the name of this type, but meh! *)
 type bracket_t =
-  | `Square
+  [ `Square
   | `Curly
   | `Angle
-  | `Round
+  | `Round ]
 
 type t =
-  | `None
-  | `Nested of bracket_t * t
+  [ `Nested of bracket_t * t
   | `Hole of string
   | `Code of string
-  | `Block of t list
+  | `Block of t list ]
+
+let rec print fragment =
+  match fragment with
+  | `Nested(_, f) -> Format.sprintf "Nested(%s)" (print f)
+  | `Hole(name) -> Format.sprintf "Hole(%s)" name
+  | `Code(s) -> Format.sprintf "Code(%s)" s
+  | `Block(fs) ->
+      let fs = String.concat "; " (List.map print fs) in
+        Format.sprintf "Block([%s])" fs
