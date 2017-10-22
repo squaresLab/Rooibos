ef892246dfb59ab6e8948f6448b3c94dc0d6629a
ChrisTimperley <christimperley@googlemail.com>
2017-10-21 23:37:22 -0400

    updated example

--- src/rooibos.ml
+++ src/rooibos.ml
@@ -3,7 +3,7 @@ open Lexer
 open Lexing
 
 let () =
-  let snippet = Lexing.from_string "x(y())" in
+  let snippet = Lexing.from_string "x(:[arga], z(:[argb], 10))" in
   let result = Parser.main Lexer.read snippet in
   let _ = print_string "sup?\n" in
 		Fragment.print result
