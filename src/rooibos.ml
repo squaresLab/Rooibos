ebb630d7d6618c7f773dc637d95bb08ec6657531
Rijnard van Tonder <rvantonder@gmail.com>
2017-10-23 19:29:57 -0400

    Proposal for inital grammar, and unifier (this time no force push, it's just hurting github)

--- src/rooibos.ml
+++ src/rooibos.ml
@@ -1,4 +1,19 @@
+open Core_kernel
+open Environment
 open Lexer
 open Lexing
+open Term
+open Unify
 
-let () = ()
+let () =
+  let matcher = "(x(:[1]()))" in
+  let source = "(x(y()))" in
+  let syntax_matcher = Parser.main Lexer.read (Lexing.from_string matcher) in
+  let syntax_source = Parser.main Lexer.read (Lexing.from_string source) in
+  Format.printf "parsed matcher: %s@." @@ Term.to_string syntax_matcher;
+  Format.printf "parsed source:  %s@." @@ Term.to_string syntax_source;
+  let environment =
+    try Unify.unify_terms (Environment.create ()) syntax_matcher syntax_source with
+    | NoUnify -> failwith @@ sprintf "Could not unify %s and %s" matcher source
+  in
+  Format.printf "%s@." @@ Environment.to_string environment
