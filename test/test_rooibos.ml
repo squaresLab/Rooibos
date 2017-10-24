75be2ef06b42333e8e8e8fbaacbb0f621ad45ff3
Rijnard van Tonder <rvantonder@gmail.com>
2017-10-23 22:52:38 -0400

    some more tests

--- test/test_rooibos.ml
+++ test/test_rooibos.ml
@@ -1,10 +1,41 @@
 open OUnit2
+open Lexing
+
 open Rooibos
 
-let test_parser ctxt =
-  let (!) s = Parser.main Lexer.read (Lexing.from_string s) in
-  let matcher = !"(x(:[_]()))" in
-  ()
+let pp_position formatter lexbuf =
+  let pos = lexbuf.lex_curr_p in
+  Format.fprintf formatter "%s:%d:%d" pos.pos_fname
+    pos.pos_lnum (pos.pos_cnum - pos.pos_bol + 1)
+
+let (!) s =
+  let lexbuf = Lexing.from_string s in
+  try Parser.main Lexer.read lexbuf |> ignore with
+  | Parser.Error ->
+    failwith @@ Format.asprintf "%a: syntax error in %s\n" pp_position lexbuf s
+
+let assert_fails_with_message message f =
+  assert_raises (Failure message) f
+
+let test_parser _ =
+  !"";
+  !"x";
+  !"x()x";
+  !"()x()";
+  !"{a{[x[z]y]({})}b}d";
+  !"(x(:[_]()))";
+
+  assert_fails_with_message
+    ":1:2: syntax error in {\n"
+    (fun () -> !"{");
+
+  assert_fails_with_message
+    ":1:11: syntax error in (x(:[_]())\n"
+    (fun () -> !"(x(:[_]())");
+
+  assert_fails_with_message
+    "FIXME TWO OR MORE HOLES SHOULD FAIL"
+    (fun () -> !":[_]:[_]:[_]")
 
 let suite =
   "test" >::: [
