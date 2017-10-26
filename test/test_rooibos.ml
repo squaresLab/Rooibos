f225ebf6d530f3939082504de49760c1e8b571c6
Rijnard van Tonder <rvantonder@gmail.com>
2017-10-26 19:03:14 -0400

    real substitution use cases

--- test/test_rooibos.ml
+++ test/test_rooibos.ml
@@ -16,6 +16,8 @@ let (!) s =
   | Parser.Error ->
     failwith @@ Format.asprintf "%a: syntax error in %s\n" pp_position lexbuf s
 
+let unify = Unify.unify_terms (Environment.create ())
+
 let assert_fails_with_message message f =
   assert_raises (Failure message) f
 
@@ -55,8 +57,6 @@ let test_parser _ =
 
 
 let test_unify _ =
-  let unify = Unify.unify_terms (Environment.create ()) in
-
   let env = unify !"(x(:[1]()))" !"(x(y()))" in
   assert_equal
     (Term.Const "y")
@@ -102,6 +102,83 @@ let test_unify _ =
     ([!"a,b,c"; !":"])
     ([Environment.lookup env ("1",0); Environment.lookup env ("2",0)])
 
+(** OMG I DONT KNOW *)
+let what_should_this_give _ =
+  let _ = unify !"x = :[1];" !"x = foo; x = bar;" in
+  ()
+
+
+let test_match_use_cases _ =
+  let env = unify !"strcpy(dst,src)" !"strcpy(:[1],src)" in
+  assert_equal
+    (!"dst")
+    (Environment.lookup env ("1",0));
+
+  let env = unify !"strcpy(dst,src)" !"strcpy(:[1],:[2])" in
+  assert_equal
+    ([!"dst"; !"src"])
+    ([Environment.lookup env ("1",0); Environment.lookup env ("2",0)])
+
+
+let format s =
+  let s = s |> String.chop_prefix_exn ~prefix:"\n"
+  in
+  let leading_indentation =
+    Option.value_exn (String.lfindi s ~f:(fun _ c -> c <> ' '))
+  in
+  s
+  |> String.split ~on:'\n'
+  |> List.map ~f:(Fn.flip String.drop_prefix leading_indentation)
+  |> String.concat ~sep:"\n"
+
+
+let test_strcpy_replace_use_case _ =
+  let template = "strcpy(:[1],:[2])" in
+  let env = unify !"strcpy(dst,src)" !template in
+  assert_equal
+    ("strcpy(src,dst)")
+    (Printer.to_string (Environment.substitute env !template))
+
+let test_add_if_brackets_use_case _ =
+  let matcher =
+    {|
+      if (:[1])
+        ::[2]
+    |}
+    |> format
+  in
+
+  let source =
+    {|
+      if (foo() > bar())
+        return -1;
+    |}
+    |> format
+  in
+
+  let rewrite =
+    {|
+      if (:[1]) {
+        ::[2]
+      }
+    |}
+    |> format
+  in
+
+  let result =
+    {|
+      if (foo() > bar()) {
+        return -1;
+      }
+    |}
+    |> format
+  in
+
+  let env = unify !source !matcher in
+  assert_equal
+    result
+    (Printer.to_string (Environment.substitute env !rewrite))
+
 let test_printer _ =
   let term = !"x()" in
   assert_equal
@@ -133,11 +210,14 @@ let test_printer _ =
     ("strcpy(dst,src)")
     (Printer.to_string term)
 
-  let suite =
-    "test" >::: [
-      "test_parser" >:: test_parser
-    ; "test_unify" >:: test_unify
-    ; "test_printer" >:: test_printer
-    ]
+let suite =
+  "test" >::: [
+    "test_parser" >:: test_parser
+  ; "test_unify" >:: test_unify
+  ; "test_match_use_cases" >:: test_match_use_cases
+  ; "test_strcpy_replace_use_case" >:: test_strcpy_replace_use_case
+  ; "test_add_if_brackets_use_case" >:: test_add_if_brackets_use_case
+  ; "test_printer" >:: test_printer
+  ]
 
 let () = run_test_tt_main suite
