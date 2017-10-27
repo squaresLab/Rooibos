32498c16c7304c249ae1d89c4eb8adf6d462dbdb
ChrisTimperley <christimperley@googlemail.com>
2017-10-27 17:25:22 -0400

    updated interface for Location module

--- lib/location.ml
+++ lib/location.ml
@@ -1,13 +1,16 @@
 type t =
-  { line : int
-  ; column : int
-  }
+  { offset : int }
 
 module Range = struct
   type nonrec t =
     { start_ : t
     ; end_ : t
     }
+
+  let make start stop =
+    let start = { offset = start } in
+    let stop = { offset = stop } in
+    { start_ = start; end_ = stop }
 
   let to_string _ = ""
 end
--- lib/location.mli
+++ lib/location.mli
@@ -1,15 +1,37 @@
-type t =
-  { line : int
-  ; column : int
-  }
+type t
 
 module Range : sig
+  type k = t
   type nonrec t =
     { start_ : t
     ; end_ : t
     }
 
+  (* [make start_ stop_] constructs a location range that spans all characters
+      starting from [start] till [stop], inclusive. *)
+  val make : k -> k -> t
+
+  (* [start range] returns the location at which [range] begins. *)
+  val start : t -> k
+
+  (* [stop range] returns the location at which [range] ends. *)
+  val stop : t -> k
+
   val to_string : t -> string
 end
+
+(* [make] constructs a location from a lexer position [pos] *)
+val make : Lexing.position -> t
+
+(* [offset loc] returns the character offset for location [loc] *)
+val offset : t -> int
+
+(* [line_no loc] returns the number of the line at which location
+   [loc] occurs. *)
+val line_no : t -> int
+
+(* [file_name loc] returns the name of the file at which location
+   [loc] occurs. *)
+val file_name : t -> string
 
 val to_string : t -> string
