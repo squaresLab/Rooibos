90c51b585ef549a63edc3cbd28ace99afc8af263
ChrisTimperley <christimperley@googlemail.com>
2017-12-12 00:37:54 -0500

    updated Environment to handle location data

--- lib/environment.ml
+++ lib/environment.ml
@@ -12,17 +12,21 @@ let vars env =
 let add env var term =
   List.Assoc.add ~equal:Pervasives.(=) env var term
 
-let rec lookup (env : t) (x : variable) : Term.t =
-  match List.Assoc.find ~equal:Pervasives.(=) env x with
-  | Some x -> x
-  | None -> Var x
+let rec lookup (env : t) (var : variable) : Term.t =
+  match List.Assoc.find ~equal:Pervasives.(=) env var with
+  | Some var -> var
+  (* CT: I don't really like this behaviour; I think that [lookup] should
+   * return [None] if [var] isn't found. *)
+  (* TODO: we have to pass a mock location *)
+  | None -> Var (var, Location.Range.mock)
 
 let rec substitute env = function
-  | Var x as e ->
-    (let e' = lookup env x in
+  | Var (var, _) as e ->
+    (let e' = lookup env var in
      if e = e' then e' else substitute env e')
-  | White _ | Const _ | Break as e -> e
-  | Compound (c, ls) -> Compound (c, List.map ~f:(substitute env) ls)
+  | White _ | Const _ | Break _ as e -> e
+  | Compound (c, ls, loc) ->
+      Compound (c, List.map ~f:(substitute env) ls, loc) (* TODO: hacky *)
 
 let to_string env =
     match List.filter ~f:(fun ((_, n), _) -> n = 0) env with
