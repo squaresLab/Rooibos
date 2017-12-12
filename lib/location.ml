1c557cf24fc425931417b0cadbd8970be408a2ea
ChrisTimperley <christimperley@googlemail.com>
2017-12-11 22:22:02 -0500

    implemented 'to_string' methods in Location module

--- lib/location.ml
+++ lib/location.ml
@@ -1,15 +1,22 @@
+open Core_kernel
+
 type t =
   { line : int
   ; column : int
+  ; offset : int
   }
+
+let to_string loc =
+  (Int.to_string loc.line) ^ ":" ^ (Int.to_string loc.column)
 
 module Range = struct
   type nonrec t =
-    { start_ : t
-    ; end_ : t
+    { start : t
+    ; stop : t
     }
 
-  let to_string _ = ""
+  let loc_to_s = to_string
+  let to_string range =
+    (loc_to_s range.start) ^ "::" ^ (loc_to_s range.stop)
 end
 
-let to_string _ = ""
