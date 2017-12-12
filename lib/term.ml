2a6a48d3a0567b9e661fc9ff794bbd0105a10ac3
ChrisTimperley <christimperley@googlemail.com>
2017-12-12 00:43:52 -0500

    updated implementation of Term module

--- lib/term.ml
+++ lib/term.ml
@@ -5,30 +5,36 @@ type constant = string
 type variable = string * int
 
 type t =
-  | Break
-  | White of constant
-  | Var of variable
-  | Const of constant
-  | Compound of constant * t list
+  | Break     of Location.Range.t
+  | White     of constant * Location.Range.t
+  | Var       of variable * Location.Range.t
+  | Const     of constant * Location.Range.t
+  | Compound  of constant * t list * Location.Range.t
 
 
 let rec contains term variable =
   match term with
-  | Var y -> variable = y
-  | Compound (_, ts) -> List.exists ~f:(fun term -> contains term variable) ts
+  | Var (y, _) -> variable = y
+  | Compound (_, ts, _) ->
+    List.exists ~f:(fun term -> contains term variable) ts
   | Const _
   | White _
-  | Break -> false
+  | Break _ -> false
 
-let range _ = Location.Range.mock
+let range = function
+  | Break     loc         -> loc
+  | White     (_, loc)    -> loc
+  | Var       (_, loc)    -> loc
+  | Const     (_, loc)    -> loc
+  | Compound  (_, _, loc) -> loc
 
 let rec to_string = function
-  | Break -> "CR"
-  | White w -> Format.sprintf "W(%S)" w
-  | Var (v, 0) -> "H(" ^ v ^ ")"
-  | Var (v, n) -> "H(" ^ v ^ ", " ^ (string_of_int n) ^ ")"
-  | Const c -> "C(" ^ c ^ ")"
-  | Compound (f, ls) ->
+  | Break _ -> "CR"
+  | White (w, _) -> Format.sprintf "W(%S)" w
+  | Var ((v, 0), _) -> "H(" ^ v ^ ")"
+  | Var ((v, n), _) -> "H(" ^ v ^ ", " ^ (string_of_int n) ^ ")"
+  | Const (c, _) -> "C(" ^ c ^ ")"
+  | Compound (f, ls, _) ->
     let (prefix, suffix) = match f with
       | "block" -> "<", ">"
       | _ -> ("N_" ^ f ^ "("), ")"
